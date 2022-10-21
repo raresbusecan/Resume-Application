057f367127185ea031875f2181cbfdc08d7b7d98
Rares Busecan <raresbusecan1999@gmail.com>
2022-10-21 19:54:58 +0300

    Bacground

--- src/Components/Pages/ContactPage/Contact.jsx
+++ src/Components/Pages/ContactPage/Contact.jsx
@@ -2,14 +2,10 @@ import './Contact.css'
 import emailjs from '@emailjs/browser';
 import BackgroundForm from '../../image/contact-futurist.jpg'
 import React, { useRef } from 'react';
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import Background from '../../Background/background';
 
 function ContactPage() {
-    const particlesInit = async (main) => {
-        console.log(main);
-        await loadFull(main);
-    };
+    
     const form = useRef();
     const sendEmail = (e) => {
         e.preventDefault();
@@ -22,130 +18,7 @@ function ContactPage() {
     };
     return (
         <>
-            <Particles
-                id="tsparticles"
-                init={particlesInit}
-
-                options={{
-                    "fullScreen": {
-                        "enable": true,
-                        "zIndex": 1
-                    },
-                    "particles": {
-                        "number": {
-                            "value": 100,
-                            "density": {
-                                "enable": false,
-                                "value_area": 1000
-                            }
-                        },
-                        "color": {
-                            "value": "#116466"
-                        },
-                        "shape": {
-                            "type": ["circle", "triangle", "polygon"],
-                            "options": {
-                                "sides": 10
-                            }
-                        },
-                        "opacity": {
-                            "value": 0.5,
-                            "random": false,
-                            "anim": {
-                                "enable": true,
-                                "speed": 1,
-                                "opacity_min": 0.1,
-                                "sync": false
-                            }
-                        },
-                        "size": {
-                            "value": 20,
-                            "random": false,
-                            "anim": {
-                                "enable": true,
-                                "speed": 40,
-                                "size_min": 0.1,
-                                "sync": false
-                            }
-                        },
-                        "rotate": {
-                            "value": 0,
-                            "random": true,
-                            "direction": "clockwise",
-                            "animation": {
-                                "enable": true,
-                                "speed": 5,
-                                "sync": false
-                            }
-                        },
-                        "line_linked": {
-                            "enable": false,
-                            "distance": 600,
-                            "color": "#D1EBE2",
-                            "opacity": 0.1,
-                            "width": 1
-                        },
-                        "move": {
-                            "enable": true,
-                            "speed": 2,
-                            "direction": "none",
-                            "random": false,
-                            "straight": false,
-                            "out_mode": "out",
-                            "attract": {
-                                "enable": false,
-                                "rotateX": 600,
-                                "rotateY": 1200
-                            }
-                        }
-                    },
-                    "interactivity": {
-                        "events": {
-                            "onhover": {
-                                "enable": true,
-                                "mode": ["grab"]
-                            },
-                            "onclick": {
-                                "enable": true,
-                                "mode": "bubble"
-                            },
-                            "resize": true
-                        },
-                        "modes": {
-                            "grab": {
-                                "distance": 400,
-                                "line_linked": {
-                                    "opacity": 0.1
-                                }
-                            },
-                            "bubble": {
-                                "distance": 400,
-                                "size": 30,
-                                "duration": 0.5,
-                                "opacity": 0.1,
-                                "speed": 3
-                            },
-                            "repulse": {
-                                "distance": 200
-                            },
-                            "push": {
-                                "particles_nb": 4
-                            },
-                            "remove": {
-                                "particles_nb": 2
-                            }
-                        }
-                    },
-                    "retina_detect": true,
-                    "background": {
-                        "color": "transparent",
-                        "image": "",
-                        "position": "50% 50%",
-                        "repeat": "repeat",
-                        "size": "cover"
-                    }
-                }}
-            />
+        <Background/>
             <div >
                 <img className='background' src={BackgroundForm}></img>
                 <form className='form' ref={form} onSubmit={sendEmail}>
--- src/Components/Pages/SkillPage/Skill.jsx
+++ src/Components/Pages/SkillPage/Skill.jsx
@@ -12,140 +12,13 @@ import { Carousel } from 'react-responsive-carousel';
 import responsiveImage from '../../image/responsive.png'
 import sbImage from '../../image/simpleblog.png'
 import dbImage from '../../image/dogblog.png'
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import Background from '../../Background/background';
 import BMI from '../../image/BMI.jpg'
 
 function SkillPage() {
-    const particlesInit = async (main) => {
-        console.log(main);
-        await loadFull(main);
-    };
     return (
         <>
-            <Particles
-                id="tsparticles"
-                init={particlesInit}
-                options={{
-                    "fullScreen": {
-                        "enable": true,
-                        "zIndex": 1
-                    },
-                    "particles": {
-                        "number": {
-                            "value": 100,
-                            "density": {
-                                "enable": false,
-                                "value_area": 1000
-                            }
-                        },
-                        "color": {
-                            "value": "#116466"
-                        },
-                        "shape": {
-                            "type": ["circle", "triangle", "polygon"],
-                            "options": {
-                                "sides": 10
-                            }
-                        },
-                        "opacity": {
-                            "value": 0.5,
-                            "random": false,
-                            "anim": {
-                                "enable": true,
-                                "speed": 1,
-                                "opacity_min": 0.1,
-                                "sync": false
-                            }
-                        },
-                        "size": {
-                            "value": 20,
-                            "random": false,
-                            "anim": {
-                                "enable": true,
-                                "speed": 40,
-                                "size_min": 0.1,
-                                "sync": false
-                            }
-                        },
-                        "rotate": {
-                            "value": 0,
-                            "random": true,
-                            "direction": "clockwise",
-                            "animation": {
-                                "enable": true,
-                                "speed": 5,
-                                "sync": false
-                            }
-                        },
-                        "line_linked": {
-                            "enable": false,
-                            "distance": 600,
-                            "color": "#D1EBE2",
-                            "opacity": 0.1,
-                            "width": 1
-                        },
-                        "move": {
-                            "enable": true,
-                            "speed": 2,
-                            "direction": "none",
-                            "random": false,
-                            "straight": false,
-                            "out_mode": "out",
-                            "attract": {
-                                "enable": false,
-                                "rotateX": 600,
-                                "rotateY": 1200
-                            }
-                        }
-                    },
-                    "interactivity": {
-                        "events": {
-                            "onhover": {
-                                "enable": true,
-                                "mode": ["grab"]
-                            },
-                            "onclick": {
-                                "enable": true,
-                                "mode": "bubble"
-                            },
-                            "resize": true
-                        },
-                        "modes": {
-                            "grab": {
-                                "distance": 400,
-                                "line_linked": {
-                                    "opacity": 0.1
-                                }
-                            },
-                            "bubble": {
-                                "distance": 400,
-                                "size": 30,
-                                "duration": 0.5,
-                                "opacity": 0.1,
-                                "speed": 3
-                            },
-                            "repulse": {
-                                "distance": 200
-                            },
-                            "push": {
-                                "particles_nb": 4
-                            },
-                            "remove": {
-                                "particles_nb": 2
-                            }
-                        }
-                    },
-                    "retina_detect": true,
-                    "background": {
-                        "color": "transparent",
-                        "image": "",
-                        "position": "50% 50%",
-                        "repeat": "repeat",
-                        "size": "cover"
-                    }
-                }}
-            />
+        <Background/>
             <div className='skill-container'>
                 <div className='skill'>
                     <div className='html-skill'>
@@ -162,7 +35,7 @@ function SkillPage() {
                     </div>
                     <div className='react-skill'>
                         <img id='s-image' src={reactimage} ></img>
-                        <ProgressBar className='bar' style={{ backgroundColor: 'transparent' }} animated striped variant="info" now={50} label={`${50}%`} />
+                        <ProgressBar className='bar' style={{ backgroundColor: 'transparent' }} animated striped variant="info" now={40} label={`${40}%`} />
                     </div>
                 </div>
                 <div className='carousel'>
